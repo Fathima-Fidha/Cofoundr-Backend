8be9a7ec85953ec95b22c0a0f8fc4c0e5f69721b
Fathima-Fidha <fathimafidha2903@gmail.com>
2025-01-16 12:36:07 +0530

    Authentication backend

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/auth.user');
 const jwt = require('jsonwebtoken');
 
 // Register a new user
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser } = require('../controllers/authController');
+const { registerUser } = require('../controllers/auth.controller');
 const router = express.Router();
 
 // Register route
