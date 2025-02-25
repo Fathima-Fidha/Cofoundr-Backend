f740c91019ed2e40fe76f90af38f74285408a28d
Fathima-Fidha <fathimafidha2903@gmail.com>
2025-02-25 14:11:36 +0530

    notification completed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,13 @@
 // firebase.js
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
+import { getMessaging } from 'firebase-admin/messaging';
 dotenv.config();
 const serviceAccount = {
   type: "service_account",
-  project_id: "pushnotifications-ede6d",
+  project_id: "pushnotifications-f2d69",
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -15,25 +16,32 @@ const serviceAccount = {
   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
   universe_domain: "googleapis.com",
 };
-// Path to your Firebase Admin SDK JSON file
-console.log(process.env.PORT, '678');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 export const sendPushNotification = async (token, message) => {
-  console.log(token, message, 'loi');
-  
+  console.log(token, message, 'Notification Data');
+
+  if (!token || !message) {
+    console.error("Invalid token or message");
+    return;
+  }
+
   try {
     const payload = {
+      token:token, // Device token
       notification: {
         title: "New Notification",
         body: message,
       },
     };
-    await admin.messaging().sendToDevice(token, payload);
-    console.log("Push notification sent successfully");
+
+    console.log(payload, "payload")
+
+    const response = await admin.messaging().send(payload);
+    console.log("Push notification sent successfully:", response.responses[0].error);
   } catch (error) {
     console.error("Error sending push notification:", error);
   }
