fbb7c622f5b0a76ed3fc59fcc4ae683a77dc73e8
hana <vhana8844@gmail.com>
2025-03-03 16:09:34 +0530

    Integration completed

--- src/routes/singleProfile.route.js
+++ src/routes/singleProfile.route.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { getSingleProfile } from '../controllers/singleProfiles.controller.js';
 
 const router = express.Router();
-router.get('/singleprofile/:id', getSingleProfile);
+router.get('/singleprofile/:userId', getSingleProfile);
 
 export default router;
